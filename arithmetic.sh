7540e3fc397a6093695a051fe4272f1e6de827ea
Yogesh <ypadalkar95@gmail.com>
2023-02-05 12:05:05 +0530

    Displayinputs

---
+++ arithmetic.sh
@@ -0,0 +1,10 @@
+echo " Enter the first input"
+read input1
+echo "Enter the second input"
+read input2
+echo "Enter the third input"
+read input3
+echo "first input is : $input1"
+echo "Second input is : $input2"
+echo "Third input is : $input3"
+
